9f7af6be738de2a1b1c612e42405cdb13f102700
bradleyalexa <bradleyalex0405@gmail.com>
2026-04-13 00:34:46 +0700

    fix gsc

--- src/app/constants/links.ts
+++ src/app/constants/links.ts
@@ -20,23 +20,23 @@ export const EMAIL_CARE = "contact@michaelhuang.id";
 export const LOCATION_ADDRESS = "Michael Huang Building - Ruko Mutiara Taman Palem Blok C8 No 23 - 26 Cengkareng Timur Cengkareng Jakarta Barat, 11730 Jl Outer Ring Road";
 //export const GOOGLE_MAPS_SEARCH_URL = `https://maps.google.com/?q=${encodeURIComponent(LOCATION_ADDRESS)}`;
 export const GENERAL_MAPS_URL = `https://maps.app.goo.gl/tfV2tHeX9UWTXP3e7`;
-export const GOOGLE_MAPS_EMBED_URL = "https://www.google.com/maps?q=Michael,+S.H.,+S.T.,+M.Kn.,+Komplek+Mall,+Perum+Ruko+Mutiara+Taman+Palem+Blok+C8.+23+Jalan+Outer+Ring+Road,+Sebarisan+Hotel+Aston+Cengkareng,+Taman+Palem+Jakarta+Barat+DKI+Jakarta+11730+ID,+Jl.+Kamal+Raya+Belakang,+Cengkareng+Tim.&output=embed";
+export const GOOGLE_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.93884262891!2d106.73066707400585!3d-6.13891866018095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7fd6383a5cd%3A0xfd4228bd9c682cda!2sMichael%2C%20S.H.%2C%20S.T.%2C%20M.Kn.!5e0!3m2!1sen!2sid!4v1776011935636!5m2!1sen!2sid";
 
 //LAW
 export const LAW_FIRM_MAPS_URL = "https://maps.app.goo.gl/cEP6w3ixvTqCVPDi9";
-export const LAW_FIRM_MAPS_EMBED_URL = "https://www.google.com/maps?q=Michael,+Antonius+%26+Co+Law+Firm&output=embed";
+export const LAW_FIRM_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9587122898038!2d106.72863377400589!3d-6.136249860156894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d52f296aadb%3A0x22964f8c19996f73!2sMichael%2C%20Antonius%20%26%20Co%20Law%20Firm!5e0!3m2!1sen!2sid!4v1776011857178!5m2!1sen!2sid";
 export const PPAT_MAPS_URL = "https://maps.app.goo.gl/4hPmVTSgwvbKSHyw5";
-export const PPAT_MAPS_EMBED_URL = "https://www.google.com/maps?q=Michael,+S.H.,+S.T.,+M.Kn.,+Komplek+Mall+Mutiara+Taman+Palem+Blok+C8&output=embed";
+export const PPAT_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.93884262891!2d106.73066707400585!3d-6.13891866018095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7fd6383a5cd%3A0xfd4228bd9c682cda!2sMichael%2C%20S.H.%2C%20S.T.%2C%20M.Kn.!5e0!3m2!1sen!2sid!4v1776011935636!5m2!1sen!2sid";
 export const NOTARIS_MAPS_URL = "https://maps.app.goo.gl/4hPmVTSgwvbKSHyw5";
-export const NOTARIS_MAPS_EMBED_URL = "https://www.google.com/maps?q=Michael,+S.H.,+S.T.,+M.Kn.,+Komplek+Mall+Mutiara+Taman+Palem+Blok+C8&output=embed";
+export const NOTARIS_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.93884262891!2d106.73066707400585!3d-6.13891866018095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7fd6383a5cd%3A0xfd4228bd9c682cda!2sMichael%2C%20S.H.%2C%20S.T.%2C%20M.Kn.!5e0!3m2!1sen!2sid!4v1776011935636!5m2!1sen!2sid";
 
 
 export const CHURCH_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9387291412036!2d106.7332412!3d-6.1389339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7276ef2336d%3A0xf9a9b8af9cd69bf5!2sGlory%20Ministry%20Church%20(GMC)!5e0!3m2!1sen!2sid!4v1775758973376!5m2!1sen!2sid";
 export const CHURCH_MAPS_URL = "https://maps.app.goo.gl/BN1d4wCPs4wQpNUv6?g_st=aw";
 
 export const KOST_MERUYA_MAPS_URL = "https://maps.app.goo.gl/m4YkjueLEbRPjDWj7";
-export const KOST_MERUYA_MAPS_EMBED_URL = "https://www.google.com/maps?q=KoolKost+near+Mercu+Buana+University,+-6.2133369,106.7377795&output=embed";
+export const KOST_MERUYA_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3813607290394!2d106.73520457400696!3d-6.2133315608571085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7bd47a14ebf%3A0x7a0a142979754987!2sKoolKost%20near%20Mercu%20Buana%20University!5e0!3m2!1sen!2sid!4v1776012567345!5m2!1sen!2sid";
 export const KOST_GEMINI_MAPS_URL = "https://maps.app.goo.gl/rbx37dyEodS8GvhRA";
 export const WARTEG_MAPS_URL = "https://maps.app.goo.gl/rbx37dyEodS8GvhRA";
-export const WARTEG_MAPS_EMBED_URL = "https://www.google.com/maps?q=Warteg+%26+Kos+Nyaman+Gemini,+-6.1362552,106.7312087&output=embed";
-export const KOST_GEMINI_MAPS_EMBED_URL = "https://www.google.com/maps?q=Warteg+%26+Kos+Nyaman+Gemini,+-6.1362552,106.7312087&output=embed";
+export const WARTEG_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9587122898038!2d106.72863377400589!3d-6.136249860156894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1dc8bd53c531%3A0xe963d17829418e55!2sWarteg%20%26%20Kos%20Nyaman%20Gemini!5e0!3m2!1sen!2sid!4v1776012483329!5m2!1sen!2sid";
+export const KOST_GEMINI_MAPS_EMBED_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9587122898038!2d106.72863377400589!3d-6.136249860156894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1dc8bd53c531%3A0xe963d17829418e55!2sWarteg%20%26%20Kos%20Nyaman%20Gemini!5e0!3m2!1sen!2sid!4v1776012483329!5m2!1sen!2sid";
